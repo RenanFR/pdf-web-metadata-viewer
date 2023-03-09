1775b5a7faaf7331d74d0b7e4c8ab9ea9e976600
Renan Fernandes Rodrigues <x_fernare1@corp.onespan.com>
2023-03-09 12:04:09 -0300

    Add test for extracting document text method

--- src/test/java/com/onespan/pdf/web/metadata/viewer/PDFTronServiceTest.java
+++ src/test/java/com/onespan/pdf/web/metadata/viewer/PDFTronServiceTest.java
@@ -14,10 +14,10 @@ import com.onespan.pdf.web.metadata.viewer.service.PDFService;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 public class PDFTronServiceTest {
-	
+
 	InputStream demoPdfInputStream;
 	PDFService pdfService;
-	
+
 	@Before
 	public void setup() throws Exception {
 		demoPdfInputStream = getClass().getClassLoader().getResourceAsStream("PB-91275 - Demo PDF.pdf");
@@ -28,11 +28,25 @@ public class PDFTronServiceTest {
 	public void shouldGetNumberOfPagesFromDocument() throws Exception {
 		assertEquals(1L, pdfService.getPages());
 	}
-	
+
 	@Test
 	public void shouldGetDocumentPDFVersion() throws Exception {
 		assertEquals("1.7", pdfService.getPDFVersion());
+	}
 
+	@Test
+	public void shouldGetDocumentText() throws Exception {
+		String documentRawText = """
+				PDF for PB-91275This PDF is going to be used for PB-91275 demo. \
+				This demo:•Must have:oExtract Information about Text Fields (Size of the Field,Font Name, Font Size)\
+				oReplace a Text on the Text FieldoInject a Signature Field.oSigning one Signature.\
+				•Good to Have:a.PDF validation (it is a PDF file, is a valid structure)b.\
+				Verify if the document is encryptedc.Extract signatures informationd.\
+				Replace a Text from a ParagraphThis is a new paragraph to be replaced. \
+				It has a different fontText field that will have information extracted:\
+				Text field that needs to be edited:Inject signature field:\
+				Signature to be signed:Signature already signed:""";
+		assertEquals(documentRawText, pdfService.getDocumentRawText().toString());
 	}
 
 }
