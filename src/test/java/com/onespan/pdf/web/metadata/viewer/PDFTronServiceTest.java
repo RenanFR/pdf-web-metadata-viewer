ceda24496f73cbaed7be9e1df541725a5e78c8ce
Renan Fernandes Rodrigues <x_fernare1@corp.onespan.com>
2023-03-09 11:54:24 -0300

    Second test for PDF version and PDFTron

--- src/test/java/com/onespan/pdf/web/metadata/viewer/PDFTronServiceTest.java
+++ src/test/java/com/onespan/pdf/web/metadata/viewer/PDFTronServiceTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertEquals;
 
 import java.io.InputStream;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -13,12 +14,25 @@ import com.onespan.pdf.web.metadata.viewer.service.PDFService;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 public class PDFTronServiceTest {
+	
+	InputStream demoPdfInputStream;
+	PDFService pdfService;
+	
+	@Before
+	public void setup() throws Exception {
+		demoPdfInputStream = getClass().getClassLoader().getResourceAsStream("PB-91275 - Demo PDF.pdf");
+		pdfService = PDFService.getByLibrary(PdfLibrary.PDFTRON, demoPdfInputStream, null, null);
+	}
 
 	@Test
 	public void shouldGetNumberOfPagesFromDocument() throws Exception {
-		InputStream demoPdfInputStream = getClass().getClassLoader().getResourceAsStream("PB-91275 - Demo PDF.pdf");
-		PDFService pdfService = PDFService.getByLibrary(PdfLibrary.PDFTRON, demoPdfInputStream, null, null);
 		assertEquals(1L, pdfService.getPages());
+	}
+	
+	@Test
+	public void shouldGetDocumentPDFVersion() throws Exception {
+		assertEquals("1.7", pdfService.getPDFVersion());
+
 	}
 
 }
