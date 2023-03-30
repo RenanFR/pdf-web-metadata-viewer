22ebeef1be8c2270cb6bb6634925a1a8967137eb
Renan Fernandes Rodrigues <x_fernare1@corp.onespan.com>
2023-03-30 11:26:34 -0300

    Fix messages

--- src/main/java/com/onespan/pdf/web/metadata/viewer/util/WebConfig.java
+++ src/main/java/com/onespan/pdf/web/metadata/viewer/util/WebConfig.java
@@ -1,12 +1,16 @@
 package com.onespan.pdf.web.metadata.viewer.util;
 
+import java.util.Locale;
+
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -25,7 +29,18 @@ public class WebConfig implements WebMvcConfigurer {
 
 	@Bean(name = "localeResolver")
 	public LocaleResolver getLocaleResolver() {
-		return new CookieLocaleResolver();
+		SessionLocaleResolver sessionLocaleResolver = new SessionLocaleResolver();
+		sessionLocaleResolver.setDefaultLocale(Locale.ENGLISH);
+		return sessionLocaleResolver;
+	}
+
+	@Bean
+	public MessageSource messageSource() {
+		ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+
+		messageSource.setBasename("classpath:messages");
+		messageSource.setDefaultEncoding("UTF-8");
+		return messageSource;
 	}
 
 }
